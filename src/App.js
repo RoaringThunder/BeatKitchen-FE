2480678af239e52ccc2d5e5efaa76592cc9b551f
EOIN HOLLAND <eoinhwork@gmail.com>
2023-07-23 22:09:27 +0100

    Redesigned login page, removed modal andded panel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Header from "./Header";
 import React from "react";
 import Home from "./Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -7,14 +6,14 @@ import { AuthContextProvider } from "./AuthContext";
 function App() {
   return (
     <AuthContextProvider>
-      <div className="Main">
-        {/* <Header /> */}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+      {/* <div className="Main"> */}
+      {/* <Header /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </div> */}
     </AuthContextProvider>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,28 @@
 import React, { useEffect, useState, useContext } from "react";
-import Modal from "./Modal";
 import Login from "./Login.css";
 import AuthContext from "./AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faCheckCircle,
+  faTimesCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   const { CheckCookie } = useContext(AuthContext);
 
-  const [showSignUp, setShowSignUp] = useState(true);
-  const [showSignIn, setShowSignIn] = useState(true);
-
-  const [showAccessModal, setShowAccessModal] = useState(false);
-  const [activeTab, setActiveTab] = useState("signin");
+  const [activeTab, setActiveTab] = useState("login");
   const [loading, setLoading] = useState(true);
   const [animating, setAnimating] = useState(true);
+  const [showBadPassword, setShowBadPassword] = useState(false);
 
-  const onHideModal = () => {
-    setShowSignIn(true);
-    setShowSignUp(true);
-    setShowAccessModal(false);
-  };
-
-  const onClickSignIn = () => {
-    setActiveTab("signin");
-    // setShowSignIn(false);
-    // setShowSignUp(false);
-    setShowAccessModal(true);
-  };
-
-  const onClickSignUp = () => {
-    setActiveTab("signup");
-    // setShowSignIn(false);
-    // setShowSignUp(false);
-    setShowAccessModal(true);
-  };
+  const [formData, setFormData] = useState({
+    loginEmail: "",
+    loginPassword: "",
+    registerFirstname: "",
+    registerEmail: "",
+    registerPassword: "",
+    registerConfirmPassword: "",
+  });
 
   const checkCookie = () => {
     setLoading(true);
@@ -65,50 +55,202 @@ function Home() {
     return () => clearTimeouts();
   }, []);
 
-  showAccessModal
-    ? (document.body.style.overflow = "hidden")
-    : (document.body.style.overflow = "unset");
+  const loginHandler = async () => {
+    const formDataExistingUser = {
+      email: formData.loginEmail,
+      password: formData.loginPassword,
+    };
+    await loginApiCall(formDataExistingUser)
+      .then((response) => {
+        if (response.status === true) {
+        } else {
+        }
+      })
+      .catch((e) => {})
+      .finally(() => {});
+  };
+
+  const signUpHandler = () => {
+    if (formData.registerPassword !== formData.registerConfirmPassword) {
+      setShowBadPassword(true);
+      return;
+    }
+    const formDataNewUser = {
+      email: formData.registerEmail,
+      password: formData.registerPassword,
+      firstname: formData.registerFirstname,
+    };
+    UserFunctionClass.SignUp(formDataNewUser)
+      .then((response) => {
+        if (response.status === true) {
+          hide();
+        } else {
+        }
+      })
+      .catch((e) => {})
+      .finally(() => {});
+  };
+
+  const onChangeFormData = (e) => {
+    let field = e.target.id;
+    setFormData((prevState) => ({
+      ...prevState,
+      [field]: e.target.value,
+    }));
+  };
+
+  const onChangeTab = (e) => {
+    setActiveTab(e.target.id);
+  };
+
+  document.body.style.overflow = "hidden";
 
   return (
     <>
-      <Modal
-        show={showAccessModal}
-        hide={onHideModal}
-        tab={activeTab}
-        setActiveTab={setActiveTab}
-      ></Modal>
-
-      <div className="App-Header-bar">
+      <div className="login">
         {!animating && (
           <>
-            {!loading ? (
+            {loading && (
               <>
-                <span
-                  className={`Login ${showSignIn ? "" : "hidden"}`}
-                  onClick={onClickSignIn}
-                >
-                  LOGIN
-                </span>
-                <span
-                  className={`Sign-Up ${showSignUp ? "" : "hidden"}`}
-                  onClick={onClickSignUp}
-                >
-                  SIGN UP
-                </span>
+                <div className="App-Header-bar">{<span>Loading</span>}</div>
               </>
-            ) : (
-              <>{<span>Loading</span>}</>
+            )}
+            {!loading && (
+              <>
+                <div className="login-body">
+                  <div className="col-xs-12 col-sm-6 col-m-12 col-l-12 login-form">
+                    <div className="login-form-header">
+                      <h3>Salamander.io</h3>
+                      <br />
+                      <p>
+                        Allow family and friends to take control of the party
+                        today, with Salamander.io!
+                      </p>
+                    </div>
+                    <div className="login-panel">
+                      <div className="login-panel-options">
+                        <span
+                          className={`${activeTab === "login" ? "active" : ""}`}
+                          id="login"
+                          onClick={(e) => onChangeTab(e)}
+                        >
+                          Login
+                        </span>
+                        <span
+                          className={`${
+                            activeTab === "register" ? "active" : ""
+                          }`}
+                          id="register"
+                          onClick={(e) => onChangeTab(e)}
+                        >
+                          Register
+                        </span>
+                      </div>
+                      <form>
+                        <div className="form-group">
+                          {activeTab === "login" ? (
+                            <>
+                              <span>Email:</span>
+                              <input
+                                type="text"
+                                id="loginEmail"
+                                name="loginEmail"
+                                placeholder="Email"
+                                onChange={(e) => onChangeFormData(e)}
+                              />
+                              <span>Password:</span>
+
+                              <input
+                                type="text"
+                                id="loginPassword"
+                                name="loginPassword"
+                                placeholder="Password"
+                                value={
+                                  formData.loginPassword.length > 0
+                                    ? "*".repeat(formData.loginPassword.length)
+                                    : ""
+                                }
+                                onChange={(e) => onChangeFormData(e)}
+                              />
+                            </>
+                          ) : (
+                            <>
+                              <span>Firstname:</span>
+                              <input
+                                type="text"
+                                id="registerFirstname"
+                                name="registerFirstname"
+                                placeholder="Firstname"
+                                onChange={(e) => onChangeFormData(e)}
+                              />
+
+                              <span>Email:</span>
+                              <input
+                                type="text"
+                                id="registerEmail"
+                                name="registerEmail"
+                                placeholder="Email"
+                                onChange={(e) => onChangeFormData(e)}
+                              />
+                              <span>Password:</span>
+
+                              <input
+                                type="text"
+                                id="registerPassword"
+                                name="registerPassword"
+                                placeholder="Password"
+                                value={
+                                  formData.registerPassword.length > 0
+                                    ? "*".repeat(
+                                        formData.registerPassword.length
+                                      )
+                                    : ""
+                                }
+                                onChange={(e) => onChangeFormData(e)}
+                              />
+
+                              <span>Confirm Password:</span>
+                              <input
+                                type="text"
+                                id="registerConfirmPassword"
+                                name="registerConfrimPassword"
+                                placeholder="Confirm Password"
+                                value={
+                                  formData.registerConfirmPassword.length > 0
+                                    ? "*".repeat(
+                                        formData.registerConfirmPassword.length
+                                      )
+                                    : ""
+                                }
+                                onChange={(e) => onChangeFormData(e)}
+                              />
+                            </>
+                          )}
+                        </div>
+                      </form>
+                      <div className="login-panel-footer">
+                        <span>
+                          <FontAwesomeIcon icon={faCheckCircle} color="white" />
+                        </span>
+                        <span>
+                          {" "}
+                          <FontAwesomeIcon icon={faTimesCircle} color="white" />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
             )}
           </>
         )}
-      </div>
 
-      {/* <div className="vinyl-container"> */}
-      <div
-        className={"vinyl-record" + `${showAccessModal ? " expand-vinyl" : ""}`}
-      >
-        <div className="vinyl-label">
-          <div className="vinyl-hole" />
+        <div className={"vinyl-record" + `${!loading ? " expand-vinyl" : ""}`}>
+          <div className="vinyl-record-texture">
+            <div className="vinyl-label">
+              <div className="vinyl-hole" />
+            </div>
+          </div>
         </div>
       </div>
     </>
